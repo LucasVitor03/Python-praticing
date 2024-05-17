6ae8706d41546a56e47e3df63fd79e8a36ce10a0
LucasVitor03 <lucasrocha234@hotmail.com>
2024-05-16 22:58:19 -0300

    Practicing

---
+++ calculadora.py
@@ -0,0 +1,41 @@
+while True:
+    print('-'*30)
+
+    opcoes = input("Selecione uma das opções: \n[1]somar, [2]subtrair, [3]multiplicar, [4]dividir, [5]sair: ")
+    opcoes_int = int(opcoes)
+
+    if opcoes_int == 5:
+        print("Finalizando calculadora.")
+        break
+    elif opcoes_int <= 4:
+        num_1 = input("Primeiro número: ")
+        num_2 = input("Segundo número: ")
+    else:
+        print("Opção inválida! Tente novamente")
+        continue
+
+    try:
+        soma = float(num_1) + float(num_2)
+        subtracao = float(num_1) - float(num_2)
+        multiplicacao = float(num_1) * float(num_2)
+        divisao = float(num_1) / float(num_2)
+
+        if opcoes_int == 1:
+            print(soma)
+        elif opcoes_int == 2:
+            print(subtracao)
+        elif opcoes_int == 3:
+            print(multiplicacao)
+        elif opcoes_int == 4:
+            print(divisao)
+
+        sair = input("\nDeseja sair sair? [s]im ou [n]ão ").lower().startswith("s")
+
+        if sair:
+            break
+        else:
+            continue
+
+    except ValueError:
+        print("\nPrimeiro ou segundo número inválido! Tente novamente.")
+
